36264b448e84cb6a67a6e6bab5ee5c69e6927c7d
Loxymoxy <49946004+Loxymoxy@users.noreply.github.com>
2019-10-17 01:09:16 +0700

    เเก้ไข หน้า UI
    เปลี่ยนแปลง ตัวแปล

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCjJ2qn8k3f5xuyy2vx0Ujgfdc-k4bzxHY",
-    authDomain: "fooddelivery-97a09.firebaseapp.com",
-    databaseURL: "https://fooddelivery-97a09.firebaseio.com",
-    projectId: "fooddelivery-97a09",
-    storageBucket: "fooddelivery-97a09.appspot.com",
-    messagingSenderId: "879005611928",
-    appId: "1:879005611928:web:1a8678ec14f91016185ded",
-    measurementId: "G-VYW6YXH3JE"
+  apiKey: "AIzaSyCjJ2qn8k3f5xuyy2vx0Ujgfdc-k4bzxHY",
+  authDomain: "fooddelivery-97a09.firebaseapp.com",
+  databaseURL: "https://fooddelivery-97a09.firebaseio.com",
+  projectId: "fooddelivery-97a09",
+  storageBucket: "fooddelivery-97a09.appspot.com",
+  messagingSenderId: "879005611928",
+  appId: "1:879005611928:web:1a8678ec14f91016185ded",
+  measurementId: "G-VYW6YXH3JE"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -16,34 +16,64 @@ firebase.analytics();
 
 var provider = new firebase.auth.GoogleAuthProvider();
 $('#gmailbtn').click(function () {
-    console.log("wasd");
-    
+  console.log("wasd");
+  
 });
 
 document.addEventListener('init', function (event) {
-    var page = event.target;
-  
-  
-    if (page.id === 'homePage') {
-      console.log("homePage");
-  
-      $("#menubtn").click(function () {
-        $("#sidemenu")[0].open();
+
+  var page = event.target;
+
+  if (page.id === 'menuPage') {
+    console.log("menuPage");
+
+    $("#menubtn").click(function () {
+      $("#sidemenu")[0].open();
+    });
+  }
+//<-----------------login----------------->
+
+if (page.id === 'loginPage') {
+console.log("loginPage");
+$("#register").click(function () {
+  $("#content")[0].load("register.html");
+});
+}
+
+
+if (page.id === 'register') {
+console.log("register");
+$("#cancel").click(function () {
+  $("#content")[0].load("login.html");
+});
+}
+
+
+
+    $("#gmailbtn").click(function () {
+
+      console.log("Google");
+
+      firebase.auth().signInWithPopup(provider).then(function (result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+          $("#content")[0].load("menu.html");
+      }).catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
       });
+    });
   
-      $("#carousel").empty();
-      db.collection("recommended").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {       
-          var item = `<ons-carousel-item modifier="nodivider" id="item${doc.data().id}" class="recomended_item">
-              <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
-              </div>
-              <div class="recomended_item_title" id="item1_${doc.data().id}">${doc.data().name}</div>
-          </ons-carousel-item>`
-          $("#carousel").append(item);
-        });
-      });
-    }
-  
+
     if (page.id === 'menuPage') {
       console.log("menuPage");
   
@@ -53,75 +83,8 @@ document.addEventListener('init', function (event) {
       });
   
       $("#home").click(function () {
-        $("#content")[0].load("home.html");
+        $("#content")[0].load("menu.html");
         $("#sidemenu")[0].close();
       });
     }
-  
-    if (page.id === 'loginPage') { 
-      console.log("loginPage");
-  
-    //   $("#backhomebtn").click(function () {
-    //     $("#content")[0].load("home.html");
-    //   });
-
-      $("#gmailbtn").click(function () {
-
-        console.log("Google");
-
-        firebase.auth().signInWithPopup(provider).then(function (result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            // ...
-            $("#content")[0].load("menu.html");
-        }).catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-        });
-      });
-
-    }
-  });
-
-//         function facebooklogin() {
-//         firebase.auth().signInWithPopup(provider).then(function(result) {
-//   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-//   var token = result.credential.accessToken;
-//   // The signed-in user info.
-//   var user = result.user;
-//   // ...
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
-//         }
-
-// $("#gmailbtn").click(function () {
-
-// var provider = new firebase.auth.GoogleAuthProvider();
-
-//   firebase.auth().signInWithPopup(provider).then(function(result) {
-
-//     content.load('tabbar.html')
-//             .then(menu.close.bind(menu));
-
-
-//   }).catch(function(error) {
-//       console.log(error);
-//   });
-
-// })
+});
